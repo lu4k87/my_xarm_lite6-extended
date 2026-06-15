22c28ed8e298904d83dccc1c4427e219e192f9df
lu4k87 <lu4k87@live.de>
2026-06-15 12:49:10 +0200

    ui: Rename ZED TF Live Tuner references to Pointcloud TF Tuner

--- src/my_3d_vision_bringup/scripts/pointcloud_tf_tuner.py
+++ src/my_3d_vision_bringup/scripts/pointcloud_tf_tuner.py
@@ -19,7 +19,7 @@ class TFTunerGUI(QWidget):
         self.timer.start(50) # 20 Hz
 
     def initUI(self):
-        self.setWindowTitle('ZED Camera TF Tuner')
+        self.setWindowTitle('Pointcloud TF Tuner')
         layout = QVBoxLayout()
 
         # Z Slider
