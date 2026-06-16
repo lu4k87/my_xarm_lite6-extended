84088bb4d23c0318b03ad674460ce10111668240
lu4k87 <lu4k87@live.de>
2026-06-16 18:04:47 +0200

    fix: update remaining dashboard paths

--- _sh/start_test.sh
+++ _sh/start_test.sh
@@ -14,10 +14,10 @@ sleep 1
 gnome-terminal --title="[NODE]: ROS Bridge (Websocket)" -- bash -c "source /opt/ros/humble/setup.bash; source ~/dev_ws/install/setup.bash; ros2 launch rosbridge_server rosbridge_websocket_launch.xml; exec bash"
 
 # 2. Terminal: Lokaler Webserver (für UI)
-gnome-terminal --title="[PROGRAM]: Webserver (Dashboard UI)" -- bash -c "source /opt/ros/humble/setup.bash; source ~/dev_ws/install/setup.bash; cd ~/dev_ws/src/websocket; python3 -m http.server 8080; exec bash"
+gnome-terminal --title="[PROGRAM]: Webserver (Dashboard UI)" -- bash -c "source /opt/ros/humble/setup.bash; source ~/dev_ws/install/setup.bash; cd ~/dev_ws/src/dashboard_monitoring; python3 -m http.server 8080; exec bash"
 
 # 3. Terminal: Workspace Analyzer
-gnome-terminal --title="[SCRIPT]: Workspace Analyzer" -- bash -c "source /opt/ros/humble/setup.bash; source ~/dev_ws/install/setup.bash; cd ~/dev_ws/src/websocket; python3 workspace_analyzer.py; exec bash"
+gnome-terminal --title="[SCRIPT]: Workspace Analyzer" -- bash -c "source /opt/ros/humble/setup.bash; source ~/dev_ws/install/setup.bash; cd ~/dev_ws/src/dashboard_monitoring; python3 workspace_analyzer.py; exec bash"
 
 # 4. Terminal: MoveIt Servo (Fake)
 gnome-terminal --title="[NODE]: MoveIt Servo (Fake)" -- bash -c "source /opt/ros/humble/setup.bash; source ~/dev_ws/install/setup.bash; ros2 launch xarm_moveit_servo lite6_moveit_servo_fake.launch.py; exec bash"
