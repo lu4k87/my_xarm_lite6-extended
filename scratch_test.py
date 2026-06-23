ab4575374e417aba3f84d6b3a659ed9390ba6c52
mk1 <mk1@my.dell>
2026-06-23 14:00:17 +0200

    chore: remove temporary scratch_test.py script

--- scratch_test.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/home/mk1/dev_ws/src/voice_command_listener/voice_command_listener')
-import voice_command_listener
-print(voice_command_listener.clean_and_map_object("den apfel"))
-print(voice_command_listener.clean_and_map_object("die rote tasse"))
-print(voice_command_listener.clean_and_map_object("eine flasche bitte"))
