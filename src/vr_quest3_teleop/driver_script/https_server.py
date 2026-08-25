2e785059399883906d6ce1b39476a822e25307af
lu4k87 <lu4k87@live.de>
2026-08-25 18:09:09 +0200

    Fix: Switch VR Teleop server to HTTP to fix mixed content WS blocking

--- src/vr_quest3_teleop/driver_script/https_server.py
+++ src/vr_quest3_teleop/driver_script/https_server.py
@@ -11,16 +11,7 @@ def main():
     
     httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
     
-    cert_file = os.path.expanduser('~/dev_ws/certs/cert.pem')
-    key_file = os.path.expanduser('~/dev_ws/certs/key.pem')
-    
-    if os.path.exists(cert_file) and os.path.exists(key_file):
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        context.load_cert_chain(certfile=cert_file, keyfile=key_file)
-        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
-        print("Serving HTTPS on port 8443 (https://localhost:8443/controller_reader.html)...")
-    else:
-        print("WARNING: Certificates not found in ~/dev_ws/certs/. Serving HTTP on port 8443...")
+    print("Serving HTTP on port 8443 (http://127.0.0.1:8443/controller_reader.html)...")
     
     try:
         httpd.serve_forever()
